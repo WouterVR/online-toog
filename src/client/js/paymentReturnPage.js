03e2642138eb937103d0325a334342c6e852516b
WouterVR <vanrompaey.wouter@gmail.com>
2021-04-29 23:38:44 +0200

    Testing on server

--- src/client/js/paymentReturnPage.js
+++ src/client/js/paymentReturnPage.js
@@ -5,12 +5,15 @@ window.location.href
 
 let paymentReference = window.location.href.replace("http://online-toog.jhdebem.be/lookupOrder/","")
 let url = "/getPaymentStatus/"+paymentReference
+alert('url is: ' + url)
 $.post(url,paymentReference,function (paymentStatus, httpStatus){
     if(httpStatus === "success"){
         let statusTextView =  $('#paymentStatus')
+        alert('Positieve reactie van server, paymentStatus is: '+ paymentStatus)
         statusTextView.empty()
         statusTextView.append(document.createTextNode("Status van je betaling: "+translatePaymentStatus(paymentStatus)))
     }else{
+        alert("Error occured while retrieving payment status")
         console.error("Error occured while retrieving payment status")
     }
 })
